46eb744c6d186c0da13f59b9770514d279259585
adhilhabeeb <132336648+Adhilhabeeb@users.noreply.github.com>
2025-08-14 11:29:07 +0530

    doi

--- src/convertimage.js
+++ src/convertimage.js
@@ -1,16 +1,30 @@
-let imagedata =   await imagetodata("../public/vite.svg");
+let imagedata =   await imagetodata("./vite.svg");
 console.log(imagedata,"functionimagaa")
 converttoimge(imagedata)
  function converttoimge(passfunc) {
-  let {canvas, width, height, data } = passfunc;
-  console.log(canvas, "canvas");
-   const dataURL = canvas.toDataURL("image/png");
-  
-    // Create and append image
-    const imgp = new Image();
-    imgp.src = dataURL;
-    console.log(imgp, "imgp");
-    document.body.appendChild(imgp);
+  let {data } = passfunc;
+
+  const newCanvas = document.createElement('canvas');
+newCanvas.width = 40;
+newCanvas.height =40;
+
+const newCtx = newCanvas.getContext('2d');
+newCtx.putImageData(data, 0, 0); // Draws the image data back
+// Get base64 PNG
+const dataURL = newCanvas.toDataURL('image/png');
+console.log(dataURL, "dataURL");
+// // Create an image element from it
+const img = new Image();
+img.style.width = '400px';
+img.style.height = '400px';
+img.style.position = 'absolute';
+img.style.objectFit = 'contain';
+img.style.top = '0';
+img.style.left = '0';
+img.src = dataURL; // Set the source to the base64 string
+img.className="imageclass"
+document.body.appendChild(img); // Display it in the page
+
 }
  async function imagetodata(imagepath){
   const img = new Image();
@@ -37,7 +51,7 @@ let imagepadata={
     // Retrieve image data
     const imgd = ctx.getImageData(0, 0, canvasSizeW, canvasSizeH);
 console.log(imgd.data, "imgd.data");
-    imagepadata.data=imgd.data;
+    imagepadata.data=imgd;
 
     resolve(imagepadata)
     // Convert to PNG base64
